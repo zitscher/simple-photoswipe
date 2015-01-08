993975ad11994e88acf5c5069955fbfad218220d
Tobias Cichon <tobiascichon@googlemail.com>
2015-01-08 23:34:31 +0100

    disables show and hide animation for now, adds img styles so large images fit into browser window, adds transparent background

--- simple-photoswipe.js
+++ simple-photoswipe.js
@@ -154,8 +154,10 @@ document.addEventListener("DOMContentLoaded", function() {
 				// history & focus options are disabled on CodePen
 				// remove these lines in real life:
 				historyEnabled: false,
-				focus: false
-
+				focus: false,
+				showHideOpacity: false,
+				showAnimationDuration: false,
+				hideAnimationDuration: false
 			};
 
 			if(disableAnimation) {
